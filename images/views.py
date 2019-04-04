5ff34d32de3fdd4db312f352881ad6def2b1dc08
Nyirabazungu <vnyanki6@gmail.com>
2019-04-04 11:25:08 +0200

    render for project

--- images/views.py
+++ images/views.py
@@ -77,3 +77,16 @@ def view_post(post_id):
     else:
         form = ProjectForm()
     return render(request,"post.html",{"form": form})
+
+def search_results(request):
+
+    if 'project' in request.GET and request.GET["project"]:
+        search_term = request.GET.get("project")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-images/search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-images/search.html',{"message":message})
